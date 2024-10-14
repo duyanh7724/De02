9b67b610f4d08ec5813a8cc42fb4ae8dd8557cbd
duyanh7724 <duyanh7724@gmail.com>
2024-10-14 15:36:01 +0700

    Add project files.

---
+++ BUS/ProductBUS.cs
@@ -0,0 +1,60 @@
+﻿using DAL.Entities;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class ProductBUS
+    {
+        private ProductDAL productDAL;
+
+        public ProductBUS()
+        {
+            productDAL = new ProductDAL();
+        }
+
+        public List<Sanpham> GetAllProducts()
+        {
+            return productDAL.GetAllProducts();
+        }
+
+        public List<LoaiSP> GetAllCategories()
+        {
+            return productDAL.GetAllCategories();
+        }
+
+        public void AddOrUpdateProduct(Sanpham product)
+        {
+            var existingProduct = productDAL.GetAllProducts().FirstOrDefault(p => p.MaSP == product.MaSP);
+            if (existingProduct != null)
+            {
+                productDAL.UpdateProduct(product);
+            }
+            else
+            {
+                productDAL.AddProduct(product);
+            }
+        }
+
+        public void DeleteProduct(string maSP)
+        {
+            productDAL.DeleteProduct(maSP);
+        }
+
+        public string GetCategoryIdByName(string name)
+        {
+            var category = productDAL.GetCategoryByName(name);
+            return category?.MaLoai;
+        }
+        public List<Sanpham> GetProductsByName(string tenSP)
+        {
+            return productDAL.GetProductsByName(tenSP);
+        }
+
+
+    }
+}
